2cc34ec2a9e288125088e31cfdcb0099a786fb3d
Vitaliy Nefyodov <vitent@gmail.com>
2017-03-04 15:47:12 +0300

    Configure route settings for linkedroute

--- core/middlewares/route.py
+++ core/middlewares/route.py
@@ -1,12 +1,13 @@
 from aiohttp.web import DynamicResource, PlainResource
 
 
-def middleware_linkedroute(route, router, **kwargs):
+def middleware_linkedroute(route, router, **settings):
     def add_route(method, path, handler, **kwargs):
         if isinstance(route.resource, DynamicResource):
             path = route.get_info()['formatter'] + path
         if isinstance(route.resource, PlainResource):
             path = route.get_info()['path'] + path
+        kwargs.update(settings)
         return router.add_route(method, path, handler, **kwargs)
 
     route.add_route = add_route
